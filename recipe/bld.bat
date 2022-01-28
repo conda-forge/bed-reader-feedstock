7aafdefe58b83c505352da8dce648dd69b7bdf3d
Carl Kadie <carlk@msn.com>
2022-01-27 16:48:22 -0800

    remove unused TEMP stuff

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,6 @@
 REM Based on bld.bat from pysyntect-feedstock
 REM https://github.com/conda-forge/pysyntect-feedstock/
 
-REM Create temp folder
-mkdir tmpbuild_%PY_VER%
-set TEMP=%CD%\tmpbuild_%PY_VER%
 REM Print Rust version
 rustc --version
 
